d551ff7795feda4b5c8c0722e068484b85ff96a9
SwatiMalik25 <swatimalik7@gmail.com>
2024-08-29 16:45:14 +0530

    sign-up page added

--- src/Components/Navbar/NavbarElements.js
+++ src/Components/Navbar/NavbarElements.js
@@ -5,7 +5,7 @@ import { NavLink as Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const Nav = styled.nav`
-    background: #63d471;
+    background: linear-gradient(to right, white, lightgrey);
     height: 85px;
     display: flex;
     justify-content: space-between;
